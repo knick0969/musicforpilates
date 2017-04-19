d25c9c1d4fe12a92750e8297aa4ebca366680e8f
Arthur <xpigue@gmail.com>
2017-04-19 22:40:59 +1000

    few menor media queries adjustments

--- js/validation.js
+++ js/validation.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-    //option A
     $("form").submit(function(e){
-       // alert('submit intercepted');
         e.preventDefault(e);
 
+        // the script will find all inputs with the class .input
         $.each($('.input'),function() {
            if ($(this).val().length == '') {
+           // it will check it's value and if its empty, a class error will be applied
             $(this).addClass('error');
            } else{
             $(this).removeClass('error');
            }
         });
     });
+
 });
