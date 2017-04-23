b65862eccaf08bea66b06205ab693a6e45a4c589
Arthur <xpigue@gmail.com>
2017-04-23 15:15:24 +1000

    blog section done (desktop) tracks list created

--- index.php
+++ index.php
@@ -18,13 +18,76 @@
 				<div class="trackImage">
 					<div class="playBtn"></div>
 				</div>
-				<a href="#" class="cta blackCta">Buy</a>
+				<div class="trackInfo">
+
+					<a href="#" class="cta blackCta">Buy</a>
+				</div>
 			</div>
 		</article>
 		<div class="greyBackground"></div>
 	</section>
 	<section class="homeTracks">
-		
+		<div class="listOfTracks container">
+			<div class="sectionTitle container">
+				<h1 class="titleh1">Music.</h1>
+				<div class="line"></div>
+				<p class="sectionText">Music description goes here hey, that's a 2 line description only. Music description goes here hey, that's a 2 line description only.</p>
+			</div>
+			<article class="homeTrack">
+				<div class="trackInfo">
+					<span class="trackTags">PILATES</span>
+					<h3 class="trackTitle">Music Title</h3>
+					<p class="trackDescription">Music description goes here hey, that's a 2 line description only.</p>
+					<a href="#" class="cta whiteCta listen">LISTEN</a>
+					<a href="#" class="cta whiteCta">BUY</a>
+				</div>
+				<img src="assets/img/pilates1.jpg" class="trackImg">
+			</article>
+			<article class="homeTrack">
+				<div class="trackInfo">
+					<span class="trackTags">PILATES</span>
+					<h3 class="trackTitle">Music Title</h3>
+					<p class="trackDescription">Music description goes here hey, that's a 2 line description only.</p>
+					<a href="#" class="cta whiteCta listen">LISTEN</a>
+					<a href="#" class="cta whiteCta">BUY</a>
+				</div>
+				<img src="assets/img/pilates1.jpg" class="trackImg">
+			</article>
+		</div>
+		<section class="freeTrack container">
+			<div class="sectionTitle">
+				<h1 class="titleh1">Get your Free Track.</h1>
+				<div class="line"></div>
+				<p class="sectionText">Enter your email to get a free copy of Mountain River, a 10min track by Music For Pilates.</p>
+			</div>
+			<div class="inputHome">
+				<form method="post">
+					<input type="text" placeholder="Enter your e-mail address" class="input required">
+					<input type="submit" class="homeSubmit">
+				</form>
+			</div>
+		</section>
+	</section>
+	<section class="featuredBlog container">
+		<article class="featuredPost">
+			<div class="sectionTitle">
+				<h1 class="titleh1">Blog.</h1>
+				<div class="line"></div>
+			</div>
+			<div class="postImg">
+				<a href="#" class="cta whiteCta">READ POST</a>
+				<img src="assets/img/pilates1.jpg">
+			</div>
+			<div class="postInfo">
+				<p class="postDate">Posted on 26/02/1995</p>
+				<p class="postTitle">How I was doing Pilates till now</p>
+				<p class="postDescription">Pilates is the new black. love it, definitely recommend people doing that hey, so good.</p>
+			</div>
+
+		</article>
+		<div class="darkBlueBackground">
+			<a href="#" class="allposts">ALL POSTS</a>
+		</div>
 	</section>
 </body>
 </html>
