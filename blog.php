3681b50091c0ad88d00f335ed786feab69288d3b
Arthur <xpigue@gmail.com>
2017-04-25 19:46:13 +1000

    blog listing page and blog read page

---
+++ blog.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Music For Pilates | Royalty Free Music for Pilates</title>
+	<?php include('includes/head.php'); ?>
+    <script src="http://w.soundcloud.com/player/api.js"></script>
+
+</head>
+<body>
+	<?php include('includes/header.php'); ?>
+
+	<section class="internalPage blogPage">
+		<div class="greyBackground">
+			<div class="sectionTitle container">
+					<h1 class="titleh1">Blog.</h1>
+					<div class="line"></div>
+					<p class="sectionText">Posts by our team at Music For Pilates</p>
+				</div>
+			<article class="blog">
+				<figure class="blogImg" id="">
+					<img src="assets/img/pilates1.jpg" class="overlayImg">
+				</figure>
+				<div class="blogInfo">
+					<p class="blogTags">
+						<span><a href="#">pilates</a></span>
+						<span><a href="#">yoga</a></span>
+						<span><a href="#">zumba</a></span>
+						<span><a href="#">jump</a></span>
+					</p>
+					<p class="blogTitle">blog title goes here</p>
+					<p class="blogDescription">blog title goes here blog title goes here blog title goes here blog title goes here blog title goes here blog title goes here. blog title goes here blog blog title goes here blog.</p>
+					<a href="blog-post-read.php" class="cta blackCta play">READ POST</a>
+				</div>
+			</article>
+			<div class="divider"></div>
+			<article class="blog">
+				<figure class="blogImg" id="">
+					<img src="assets/img/pilates1.jpg" class="overlayImg">
+				</figure>
+				<div class="blogInfo">
+					<p class="blogTags">
+						<span><a href="#">pilates</a></span>
+						<span><a href="#">yoga</a></span>
+						<span><a href="#">zumba</a></span>
+						<span><a href="#">jump</a></span>
+					</p>
+					<p class="blogTitle">blog title goes here</p>
+					<p class="blogDescription">blog title goes here blog title goes here blog title goes here blog title goes here blog title goes here blog title goes here. blog title goes here blog blog title goes here blog.</p>
+					<a href="blog-post-read.php" class="cta blackCta play">READ POST</a>
+				</div>
+			</article>
+			<div class="divider"></div>
+			<article class="blog">
+				<figure class="blogImg" id="">
+					<img src="assets/img/pilates1.jpg" class="overlayImg">
+				</figure>
+				<div class="blogInfo">
+					<p class="blogTags">
+						<span><a href="#">pilates</a></span>
+						<span><a href="#">yoga</a></span>
+						<span><a href="#">zumba</a></span>
+						<span><a href="#">jump</a></span>
+					</p>
+					<p class="blogTitle">blog title goes here</p>
+					<p class="blogDescription">blog title goes here Track title goes here Track title goes here Track title goes here Track title goes here Track title goes here. Track title goes here Track Track title goes here Track.</p>
+					<a href="blog-post-read.php" class="cta blackCta play">READ POST</a>
+				</div>
+			</article>
+			<div class="divider"></div>
+
+
+		</div>
+	</section>
+
+	<?php include('includes/footer.php'); ?>
+
+</body>
+</html>
