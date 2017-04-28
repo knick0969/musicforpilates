d31a4c7a5e17e835d4641fcba04362b31b3b40cd
Nick Kington <knick0969@live.com>
2017-04-28 11:32:41 +1000

    Added in looping script
    Added script line to create the looping of data.

--- blog-post-read.php
+++ blog-post-read.php
@@ -49,6 +49,9 @@
 			returndata = data['return'];
 			$('#resultArea').html(data['data']);
 			$('#title').html(data['return'][0]['title'])
+			/* For each loop {
+				$('#trackContainer').append('<div class="track"><img src="img/'+data['return'][i]['image']+'"><h3>'+data['return'][i]['title']+'</h3><div class="description">'+data['return'][i]['description']+'</div><div class="price">'+data['return'][i]['cost']+'</div><a href="track.php?id='+data['return'][i]['id']+'"><button class="moreInfo">More info</button></div>')
+			} */
 		},
 		error: function (xhr, ajaxOptions, thrownError){
 	        alert(xhr.statusText);
