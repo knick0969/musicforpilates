097cbe858667a047fe083ad62aa0fd7ae70b4990
Nick Kington <knick0969@live.com>
2017-04-29 09:54:41 +1000

    Old code removed.
    This better not fucking conflict again later on, but I know it will....
    GAH>

--- blog-post-read.php
+++ blog-post-read.php
@@ -49,7 +49,6 @@
 			returndata = data['return'];
 			$('#resultArea').html(data['data']);
 			$('#title').html(data['return'][0]['title']);
-			$('#content').html(data['return'][0]['content']);
 			/* For each loop {
 				$('#trackContainer').append('<div class="track"><img src="img/'+data['return'][i]['image']+'"><h3>'+data['return'][i]['title']+'</h3><div class="description">'+data['return'][i]['description']+'</div><div class="price">'+data['return'][i]['cost']+'</div><a href="track.php?id='+data['return'][i]['id']+'"><button class="moreInfo">More info</button></div>')
 			} */
